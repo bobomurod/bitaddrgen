6f6dc4c484f174969c70e3b17ee05ea6a8c1e1a7
bobomurod <bobomurod.boboev@yandex.ru>
2018-05-10 19:27:10 +0300

    commit

--- start.js
+++ start.js
@@ -1,6 +1,18 @@
 const bitcore = require('bitcore-lib')
 const bitcoreEth = require('bitcore-lib')
 
+
+var request = require('request');
+request('https://api.coinmarketcap.com/v2/ticker/1/', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var jsonArr = JSON.parse(body);
+    console.log(jsonArr.data.quotes.USD.price);
+    // console.log("group id:" + jsonArr[0].id);
+  }
+})
+
+ 
+
 const EthereumBip44 = require('ethereum-bip44')
 var privateKey = new bitcore.PrivateKey()
 console.log(privateKey)
@@ -25,3 +37,5 @@ var ethWallet = new EthereumBip44(ethkey)
 console.log("Bip44 Eth wallet addr from ethkey")
 console.log(ethWallet.getAddress(0))
 console.log(ethWallet.getAddress(1))
+
+
